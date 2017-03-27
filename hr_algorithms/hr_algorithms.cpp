0757f0b99ce9d2ce68c65468cbaad88813e42b7a
Nam Ly <nhl1285@gmail.com>
2017-03-26 23:43:32 -0700

    Added interfaces for linked list implementation

---
+++ hr_algorithms/hr_algorithms.cpp
@@ -0,0 +1,24 @@
+// hr_algorithms.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Calculate long factorials
+//
+// @param[in] int number number to calculate factorial
+//
+// @remarks factorial is printed out
+//   
+///////////////////////////////////////////////////////////////////////////////
+void long_factorial(int number)
+{
+
+}
+
+
+int main()
+{
+    return 0;
+}
+
